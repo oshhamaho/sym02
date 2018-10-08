d15bb547323e75cc8f123c3de32ff71efa792414
oshhamaho <tsymbalov97@gmail.com>
2018-10-08 16:06:03 +0200

    Twig

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,15 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+// using entity Article
+use App\Entity\Article;
 
 class AccueilController extends AbstractController
 {
     /**
-     * @Route("/", name="accueil")
+     * @Route("/accueil", name="accueil")
      */
-    public function index()
+    public function accueil()
     {
-        return $this->render('accueil/index.html.twig', [
+           // create a Doctrine manager and get back all articles
+        $article = $this->getDoctrine()->getRepository(Article::class)->findAll();
+        // create view with twig, with attribute NosArticles
+        return $this->render("accueil/accueil.html.twig",["NosArticle"=>$article]);
+    }
+    /**
+     * @Route("/debug/AccueilController", name="debugAccueilController")
+     */
+    public function debug()
+    {
+        return $this->render('accueil/debug.html.twig', [
             'controller_name' => 'AccueilController',
         ]);
     }
